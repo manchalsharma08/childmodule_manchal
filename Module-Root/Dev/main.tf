4e268fa452ee950f9c946c345fcdbf5b23559230
manchal <manchalsharma08@gmail.com>
2025-05-28 09:01:41 +0530

    correction in module source done

--- Module-Root/Dev/main.tf
+++ Module-Root/Dev/main.tf
@@ -1,22 +1,22 @@
 module "rg" {
-    source = "../../azure_resource_group"
+    source = "../../Child-Block/azure_resource_group"
     name_rg = var.project-rg 
 }
 
 
 module "storage" {
-    source = "../../azure_storge_account"
+    source = "../../Child-Block/azure_storge_account"
     stgac = var.project-st
   }
 
 
 module "vnet" {
-    source = "../../azure_virtual_network"
+    source = "../../Child-Block/azure_virtual_network"
     vnetss = var.project-vnet
   }
 
 module "snet" {
-    source = "../../azure_subnet"
+    source = "../../Child-Block/azure_subnet"
     subnetss = var.project-snet
   
 }
