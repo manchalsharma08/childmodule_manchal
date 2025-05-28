b261327d26979fc356ee2f6fc6ce72c209162c81
manchal <manchalsharma08@gmail.com>
2025-05-28 10:07:52 +0530

    dependency assign

--- Module-Root/Dev/main.tf
+++ Module-Root/Dev/main.tf
@@ -7,17 +7,19 @@ module "rg" {
 module "storage" {
     source = "../../Child-Block/azure_storge_account"
     stgac = var.project-st
+    depends_on = [ module.rg ]
   }
 
 
 module "vnet" {
     source = "../../Child-Block/azure_virtual_network"
     vnetss = var.project-vnet
+    depends_on = [ module.rg ]
   }
 
 module "snet" {
     source = "../../Child-Block/azure_subnet"
     subnetss = var.project-snet
-  
+  depends_on = [ module.vnet, module.rg ]
 }
 
